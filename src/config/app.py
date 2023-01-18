4abdcb98a2a38404a8e930cc78b9dee11b9a73a1
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2023-01-18 21:06:36 +0100

    feat(config/app): updates app title

--- src/config/app.py
+++ src/config/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from routes import root_route
 
 from config import env
+from routes import root_route
 
 app = FastAPI(
-    title="Stritter API Gateway",
+    title="Striter Aggregation Service",
     version="0.0.0",
     docs_url="/api/v1/docs/",
     redoc_url="/api/v1/redocs/",
